ea0364d20531f631a469a00a35184cd1f97cdfd0
Steven Loria <sloria1@gmail.com>
2017-09-21 21:25:50 -0400

    Randomly choose a featured room

--- web/static/index.js
+++ web/static/index.js
@@ -13,17 +13,39 @@ function fetchJSON(...args) {
   return fetch(...args).then(checkStatus).then(parseJSON);
 }
 
+function Room(data) {
+  this.createURL = data.create_url;
+  this.name = data.name;
+  this.id = data.id;
+  this.until = data.until;
+  // TODO: image
+}
+
+function randomChoice(ary) {
+  return ary[Math.floor(Math.random() * ary.length)];
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: '#app',
     data: {
       free: [],
       busy: [],
+      featured: null,
     },
     mounted() {
       fetchJSON('/api/', { credentials: 'include' })
         .then((json) => {
-          Object.assign(this, json);
+          if (json.free.length) {
+            // Choose a random room that is not a phone booth
+            this.featured = new Room(randomChoice(
+              json.free.filter((each) => !each.name.startsWith('Phone Booth'))
+            ));
+          }
+          // More free rooms
+          const moreFree = this.featured ? json.free.filter((each) => each.name !== this.featured.name) : json.free;
+          this.free = moreFree.map((each) => new Room(each));
+          this.busy = json.busy.map((each) => new Room(each));
         });
     },
     methods: {
